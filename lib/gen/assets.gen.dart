937efccfa96796a6beb15adfa5ac5f85a97f5fa8
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-05 16:24:11 +0300

    Create assets.gen.dart

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,241 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/aa.png
+  AssetGenImage get aa => const AssetGenImage('assets/images/aa.png');
+
+  /// File path: assets/images/aaa.png
+  AssetGenImage get aaa => const AssetGenImage('assets/images/aaa.png');
+
+  /// File path: assets/images/apple.png
+  AssetGenImage get apple => const AssetGenImage('assets/images/apple.png');
+
+  /// File path: assets/images/backc.png
+  AssetGenImage get backc => const AssetGenImage('assets/images/backc.png');
+
+  /// File path: assets/images/backcc.png
+  AssetGenImage get backcc => const AssetGenImage('assets/images/backcc.png');
+
+  /// File path: assets/images/backk.png
+  AssetGenImage get backk => const AssetGenImage('assets/images/backk.png');
+
+  /// File path: assets/images/backkk.png
+  AssetGenImage get backkk => const AssetGenImage('assets/images/backkk.png');
+
+  /// File path: assets/images/backkkk.png
+  AssetGenImage get backkkk => const AssetGenImage('assets/images/backkkk.png');
+
+  /// File path: assets/images/backkkkk.png
+  AssetGenImage get backkkkk =>
+      const AssetGenImage('assets/images/backkkkk.png');
+
+  /// File path: assets/images/bb.png
+  AssetGenImage get bb => const AssetGenImage('assets/images/bb.png');
+
+  /// File path: assets/images/bbb.png
+  AssetGenImage get bbb => const AssetGenImage('assets/images/bbb.png');
+
+  /// File path: assets/images/cc.png
+  AssetGenImage get cc => const AssetGenImage('assets/images/cc.png');
+
+  /// File path: assets/images/ccc.png
+  AssetGenImage get ccc => const AssetGenImage('assets/images/ccc.png');
+
+  /// File path: assets/images/dd.png
+  AssetGenImage get dd => const AssetGenImage('assets/images/dd.png');
+
+  /// File path: assets/images/ddd.png
+  AssetGenImage get ddd => const AssetGenImage('assets/images/ddd.png');
+
+  /// File path: assets/images/dra.avif
+  String get dra => 'assets/images/dra.avif';
+
+  /// File path: assets/images/draw.png
+  AssetGenImage get draw => const AssetGenImage('assets/images/draw.png');
+
+  /// File path: assets/images/ee.png
+  AssetGenImage get ee => const AssetGenImage('assets/images/ee.png');
+
+  /// File path: assets/images/eee.png
+  AssetGenImage get eee => const AssetGenImage('assets/images/eee.png');
+
+  /// File path: assets/images/facebook.png
+  AssetGenImage get facebook =>
+      const AssetGenImage('assets/images/facebook.png');
+
+  /// File path: assets/images/ff.png
+  AssetGenImage get ff => const AssetGenImage('assets/images/ff.png');
+
+  /// File path: assets/images/fff.png
+  AssetGenImage get fff => const AssetGenImage('assets/images/fff.png');
+
+  /// File path: assets/images/gg.png
+  AssetGenImage get gg => const AssetGenImage('assets/images/gg.png');
+
+  /// File path: assets/images/ggg.png
+  AssetGenImage get ggg => const AssetGenImage('assets/images/ggg.png');
+
+  /// File path: assets/images/google.png
+  AssetGenImage get google => const AssetGenImage('assets/images/google.png');
+
+  /// File path: assets/images/hh.png
+  AssetGenImage get hh => const AssetGenImage('assets/images/hh.png');
+
+  /// File path: assets/images/hhh.png
+  AssetGenImage get hhh => const AssetGenImage('assets/images/hhh.png');
+
+  /// File path: assets/images/icon.png
+  AssetGenImage get icon => const AssetGenImage('assets/images/icon.png');
+
+  /// File path: assets/images/ig.png
+  AssetGenImage get ig => const AssetGenImage('assets/images/ig.png');
+
+  /// File path: assets/images/ii.png
+  AssetGenImage get ii => const AssetGenImage('assets/images/ii.png');
+
+  /// File path: assets/images/jj.png
+  AssetGenImage get jj => const AssetGenImage('assets/images/jj.png');
+
+  /// File path: assets/images/login.png
+  AssetGenImage get login => const AssetGenImage('assets/images/login.png');
+
+  /// File path: assets/images/mobile.png
+  AssetGenImage get mobile => const AssetGenImage('assets/images/mobile.png');
+
+  /// File path: assets/images/splashBack.png
+  AssetGenImage get splashBack =>
+      const AssetGenImage('assets/images/splashBack.png');
+
+  /// File path: assets/images/underc.png
+  AssetGenImage get underc => const AssetGenImage('assets/images/underc.png');
+
+  /// List of all assets
+  List<dynamic> get values => [
+        aa,
+        aaa,
+        apple,
+        backc,
+        backcc,
+        backk,
+        backkk,
+        backkkk,
+        backkkkk,
+        bb,
+        bbb,
+        cc,
+        ccc,
+        dd,
+        ddd,
+        dra,
+        draw,
+        ee,
+        eee,
+        facebook,
+        ff,
+        fff,
+        gg,
+        ggg,
+        google,
+        hh,
+        hhh,
+        icon,
+        ig,
+        ii,
+        jj,
+        login,
+        mobile,
+        splashBack,
+        underc
+      ];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName);
+
+  final String _assetName;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
