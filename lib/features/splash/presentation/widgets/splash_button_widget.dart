cb0833b951287869bec73f11a83cff82dcd77ac3
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-06 16:23:17 +0300

    Create splash_button_widget.dart

---
+++ lib/features/splash/presentation/widgets/splash_button_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class SplashButtonWidget extends StatelessWidget {
+  final Color backgroundColor;
+  final Color borderColor;
+  final String text;
+  final Color textColor;
+
+  const SplashButtonWidget({
+    super.key,
+    required this.backgroundColor,
+    required this.borderColor,
+    required this.text,
+    required this.textColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      // TODO: Change to elevated button
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor,
+        minimumSize: Size(double.infinity, 65),
+        side: BorderSide(
+          color: borderColor,
+          width: 1,
+          style: BorderStyle.solid,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+        shadowColor: Colors.grey.withOpacity(1),
+        elevation: 2,
+      ),
+      onPressed: () {},
+      child: Text(
+        text,
+        style: TextStyle(
+          color: textColor,
+          fontSize: 20,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+    );
+  }
+}
