886d656057b1498c316f5d8b4b7c1e55a753647b
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-14 14:57:37 +0300

    Create routes.dart

---
+++ lib/core/constants/routes.dart
@@ -0,0 +1,5 @@
+const String splashViewRoute = '/';
+const String homeViewRoute = '/home';
+const String loginViewRoute = '/login';
+const String signUpViewRoute = '/sign_up';
+const String productDetailsViewRoute = '/home/product';
