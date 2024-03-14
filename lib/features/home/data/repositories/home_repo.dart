ecc8b9f6c99eedf20db550a8882d31d6bc54d325
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-14 14:57:44 +0300

    Update home_repo.dart

--- lib/features/home/data/repositories/home_repo.dart
+++ lib/features/home/data/repositories/home_repo.dart
@@ -8,26 +8,24 @@ import 'package:http/http.dart' as http;
 
 const String baseUrl = 'https://api.escuelajs.co/api/v1/';
 
-final productProvider = FutureProvider<List<Product>>((ref) async {
-  final url = Uri.parse('${baseUrl}products');
-  final response = await http.get(url);
-  if (response.statusCode == 200) {
-    final data = jsonDecode(response.body) as List;
-    return data.map((json) => Product.fromJson(json)).toList();
-  } else {
-    throw Exception('Failed to load products');
-  }
-});
-
 class HomeRepo {
   final ApiService api = ApiService();
 
   Future<List<Category>> getCategories() async {
     final response = await api.get('categories');
-    print(response.data);
     if (response.success) {
       final data = response.data as List;
       return data.map((json) => Category.fromJson(json)).toList();
+    } else {
+      throw Exception(response.error);
+    }
+  }
+
+  Future<List<Product>> getProducts() async {
+    final response = await api.get('products');
+    if (response.success) {
+      final data = response.data as List;
+      return data.map((json) => Product.fromJson(json)).toList();
     } else {
       throw Exception(response.error);
     }
